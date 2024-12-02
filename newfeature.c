de48285f42ff3da3b96e65034a4917cd12e8197d
najihyung <skwlgud99@naver.com>
2024-12-02 14:16:33 +0900

    Resolve conflict

--- newfeature.c
+++ newfeature.c
@@ -4,6 +4,7 @@ void main(){
 	char pwd[1024];
 	FILE *f = fopen("pwd-log.log",'w');
 	getcwd(pwd, sizeof(pwd));
+	printf("working dir %s\n",pwd);
 	printf("%s\n",pwd);
 	fputs("%s\n",f);
 	fclose(f);
